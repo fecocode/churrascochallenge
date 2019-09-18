bfbf5129817e097641f1e6c4e3fc8fef3b11b6b0
Franco <franco.colares08@gmail.com>
2019-09-18 08:49:39 -0300

    Init develop

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+    <footer>
+        <span>
+            Copyright 2019 - 
+        <a href="#">
+            TestCompany
+        </a>
+        </span>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+    $footer-background: #ced6db;
+    
+    footer{
+        width: 100%;
+        position: relative;
+        background: $footer-background;
+        padding: 2em 0em;
+        text-align: center;
+    }
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-container>
+        
+        <v-footer app>
+            <Footer></Footer>
+        </v-footer>
+    </v-container>
+</template>
+
+<script>
+import Footer from '../components/layout/Footer';
+
+export default {
+  components: {
+    Footer,
+  },
+};
+</script>
